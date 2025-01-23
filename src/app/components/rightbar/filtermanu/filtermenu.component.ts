bc629a87c7be92e000936924d097ccac89bdb2c5
Messi_Dev <roloo.coolman@gmail.com>
2025-01-23 22:56:45 +0100

    api service is created

--- src/app/components/rightbar/filtermanu/filtermenu.component.ts
+++ src/app/components/rightbar/filtermanu/filtermenu.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { SharedService } from "../../../services/shared.service";
 
 @Component({
   selector: "app-filtermenu",
@@ -7,9 +8,24 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 })
 export class FiltermenuComponent {
   activeFilter: string = "all";
+  // categories: any[] = []; // Array to hold categories
+  // errorMessage: string = ""; // Variable to hold error messages
 
-  @Input()
-  title = "Leftbar";
-  @Output()
-  titleClick = new EventEmitter<FiltermenuComponent>();
+  // constructor(private sharedService: SharedService) {}
+
+  // ngOnInit(): void {
+  //   this.fetchCategories(1); // Fetch categories for tenantId 1
+  //   this.sharedService.categories$.subscribe(
+  //     (data) => {
+  //       this.categories = data; // Update the local categories array
+  //     },
+  //     (error) => {
+  //       console.error("Error in subscription:", error);
+  //     }
+  //   );
+  // }
+
+  // fetchCategories(tenantId: number): void {
+  //   this.sharedService.fetchCategories(tenantId); // Call the service to fetch categories
+  // }
 }
--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Observable, BehaviorSubject } from "rxjs";
 
 // tumbnail data
 export interface ThumbnailData {
@@ -13,6 +14,27 @@ export interface ThumbnailData {
   providedIn: "root",
 })
 export class SharedService {
+  // API CALL SERVICE
+  private apiUrl = "'https://fyhubproapi.azurewebsites.net'";
+  private categoriesSubject = new BehaviorSubject<any[]>([]);
+  categories$ = this.categoriesSubject.asObservable();
+
+  // constructor(private http: HttpClient) {}
+
+  // Fetch categories from the API
+  // fetchCategories(tenantId: number): void {
+  //   const url = `${this.apiUrl}/api/Category/Category?tenantId=${tenantId}`;
+  //   this.http.get<any[]>(url).subscribe(
+  //     (data) => {
+  //       this.categoriesSubject.next(data);
+  //     },
+  //     (error) => {
+  //       console.error("Error fetching categories:", error);
+  //     }
+  //   );
+  // }
+
+  // STATE MANAGEMENT SERVICE
   // Thumbnail Methods
   private thumbnailHolder = new BehaviorSubject<ThumbnailData | null>(null);
   thumbnail$ = this.thumbnailHolder.asObservable();
