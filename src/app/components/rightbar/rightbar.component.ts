5dd270f1825500eb6cbd1837690c18369112f7b0
Messi_Dev <roloo.coolman@gmail.com>
2025-02-08 08:10:19 +0100

    update mobile response-navbar notification icon

--- src/app/components/rightbar/rightbar.component.ts
+++ src/app/components/rightbar/rightbar.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { SharedService } from "app/services/shared.service";
+import { trigger, transition, style, animate } from "@angular/animations";
 
 @Component({
   selector: "app-rightbar",
   templateUrl: "./rightbar.component.html",
   styleUrls: ["./rightbar.component.css"],
+  animations: [
+    trigger("dropAnimation", [
+      transition(":enter", [
+        style({ opacity: 0, transform: "translateY(-20px)" }),
+        animate(
+          "300ms ease-out",
+          style({ opacity: 1, transform: "translateY(0)" })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class RightbarComponent implements OnInit {
   constructor(private sharedService: SharedService) {}
