2030f62f08db14f039b3ba7a8f9fb3c64a7de524
Messi_Dev <roloo.coolman@gmail.com>
2025-02-13 06:54:18 +0100

    update layout of delivery header

--- src/app/components/delivery/pending/pending.component.ts
+++ src/app/components/delivery/pending/pending.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewChildren,
+  QueryList,
+} from "@angular/core";
+import { DeliverlistComponent } from "../deliverlist/deliverlist.component";
 
 @Component({
-  selector: 'app-pending',
-  templateUrl: './pending.component.html',
-  styleUrls: ['./pending.component.css']
+  selector: "app-pending",
+  templateUrl: "./pending.component.html",
+  styleUrls: ["./pending.component.css"],
 })
 export class PendingComponent {
+  deliverList: any[] = new Array(5);
 
+  get deliveryPendingCount(): number {
+    return this.deliverList.length;
+  }
 }
